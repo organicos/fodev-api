36b9c7720c536ef2f1e53953646ceb68bf72f2fa
Bruno da Silva joão <bruno@tzadi.com>
2015-05-20 13:47:31 -0300

    cache de 1 semana

--- router/app.js
+++ router/app.js
@@ -3,18 +3,22 @@
 module.exports=function(app, express) {
 	
 	var path = require('path');
+	var day = 86400000;
+	var week = 7 * day;
+	var month = 30 * day;
+	var year = 365 * day;
 
 	// these need to go first:
-	app.use("/app.min.css", express.static(__dirname + "../../../fodev-app/app.min.css", { maxAge: 86400000 }));
-	app.use("/app.min.js", express.static(__dirname + "../../../fodev-app/app.min.js", { maxAge: 86400000 }));
-	app.use("/assets", express.static(__dirname + "../../../fodev-app/assets", { maxAge: 86400000 }));
-	app.use("/bower_components", express.static(__dirname + "../../../fodev-app/bower_components", { maxAge: 86400000 }));
-	app.use("/components", express.static(__dirname + "../../../fodev-app/components", { maxAge: 86400000 }));
-	app.use("/partials", express.static(__dirname + "../../../fodev-app/partials", { maxAge: 86400000 }));
-	app.use("/template", express.static(__dirname + "../../../fodev-app/template", { maxAge: 86400000 }));
-	app.use("/config", express.static(__dirname + "../../../fodev-app/config", { maxAge: 86400000 }));
-	app.use("/sitemap.xml", express.static(__dirname + "../../../fodev-app/sitemap.xml", { maxAge: 86400000 }));
-	app.use("/robots.txt", express.static(__dirname + "../../../fodev-app/robots.txt", { maxAge: 86400000 }));
+	app.use("/app.min.css", express.static(__dirname + "../../../fodev-app/app.min.css", { maxAge: week }));
+	app.use("/app.min.js", express.static(__dirname + "../../../fodev-app/app.min.js", { maxAge: week }));
+	app.use("/assets", express.static(__dirname + "../../../fodev-app/assets", { maxAge: week }));
+	app.use("/bower_components", express.static(__dirname + "../../../fodev-app/bower_components", { maxAge: week }));
+	app.use("/components", express.static(__dirname + "../../../fodev-app/components", { maxAge: week }));
+	app.use("/partials", express.static(__dirname + "../../../fodev-app/partials", { maxAge: week }));
+	app.use("/template", express.static(__dirname + "../../../fodev-app/template", { maxAge: week }));
+	app.use("/config", express.static(__dirname + "../../../fodev-app/config", { maxAge: week }));
+	app.use("/sitemap.xml", express.static(__dirname + "../../../fodev-app/sitemap.xml", { maxAge: week }));
+	app.use("/robots.txt", express.static(__dirname + "../../../fodev-app/robots.txt", { maxAge: week }));
 
 	// application -------------------------------------------------------------
 	app.all('/*', function(req, res) {
