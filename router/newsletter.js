71a9ce3cda842013201c4a4a4d682475baadad5c
Bruno da Silva joão <bruno@tzadi.com>
2015-05-27 15:29:30 -0300

    filter products by category

--- router/newsletter.js
+++ router/newsletter.js
@@ -289,13 +289,136 @@ module.exports=function(app, mongoose, config, utils) {
 
     });
     
+    app.post('/v1/newsletter/signup', function(req, res) {
+        
+        Users.findOne({email: req.body.email}, function(err, user) {
+
+            if (err) {
+                
+                res.statusCode = 400;
+                    
+                res.json({
+                    type: false,
+                    data: "Erro: " + err
+                });
+                
+            } else {
+                    
+                if (user) {
+                    
+                    user.newsletter = true;
+                    
+                    user.save(function(err, new_user) {
+                            
+                        if(err) {
+                            
+                            res.statusCode = 400;
+                            
+                            res.send(err);
+                            
+                        } else {
+
+                            res.json({
+                                type: true,
+                                data: "Cadastrado realizado com sucesso!"
+                            });
+
+                        }
+
+                    });
+                        
+                } else {
+                    
+                    Users.create({
     
-    // IMPORTANTE!
-    //
-    //
-    // O GMAIL TEM LIMITE DE 2000 ENDEREÇOS POR EMAIL. LOGO, DEVE HAVER UM TRATAMENTO NA HORA DE ENVIAR OS E-MAILS PARA QUE NÃO EXCEDA ESTE LIMITE!!!
-    //
-    //    
+                            name : req.body.email,
+    
+                            email : req.body.email,
+                            
+                            newsletter : true,
+                            
+                            kind : 'newsletter',
+                            
+                            password : crypto.createHash('md5').update(req.body.email).digest('hex')
+    
+                    }, function(err, user) {
+    
+                            if (err) {
+                                
+                                res.statusCode = 400;
+    
+                                res.send(err);
+                            
+                            } else {
+                                
+                                user.password = req.body.email;
+                                
+                                send_newsletter_signup_email(user);
+                                
+                                res.json({
+                                    type: true,
+                                    data: "Cadastrado realizado com sucesso!"
+                                });
+                                    
+                            }
+    
+                    });
+
+                }
+            }
+        });
+    });
+        
+    var send_newsletter_signup_email = function(user){
+        
+        var nodemailer = require('nodemailer');
+        var path = require('path');
+        var templatesDir   = path.join(__dirname, '../templates');
+        var emailTemplates = require('email-templates');
+
+        var transporter = nodemailer.createTransport({
+            host: 'smtp.gmail.com',
+            port: 465, // 465
+            secure: true, // true
+            debug : true,
+            auth: {
+                user: 'bruno@tzadi.com',
+                pass: 'Dublin2010ireland'
+            }
+        });
+
+        emailTemplates(templatesDir, function(err, template) {
+             
+            if (err) {
+                console.log(err);
+            } else {
+              
+                template('newsletter/signup', user, function(err, html, text) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        var mailOptions = {
+                            from: 'Feira Orgânica Delivery <info@feiraorganica.com>', //sender address
+                            replyTo: "info@feiraorganica.com",
+                            to: user.email, // list of receivers
+                            cc: 'info@feiraorganica.com', // list of BCC receivers 'bruno@tzadi.com, denisefaccin@gmail.com'
+                            subject: config.envTag + 'Assinatura de newsletter',
+                            text: text,
+                            html: html
+                        };
+                        transporter.sendMail(mailOptions, function(error, info){
+                            if(error){
+                                console.log(error);
+                            }else{
+                                console.log('Message sent: ' + info.response);
+                            }
+                        });
+                    }
+                });
+            }
+        });
+    }
+    
     var send_newsletter = function(mailData){
         
         utils.sendMail({
