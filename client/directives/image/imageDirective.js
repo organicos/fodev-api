1ea8ecebd1a8ac18ff38dd21857669e5ca16515f
Bruno da Silva joão <bruno@tzadi.com>
2016-05-18 20:45:35 -0300

    resizimage funcionando - apenas replicar

--- client/directives/image/imageDirective.js
+++ client/directives/image/imageDirective.js
@@ -14,6 +14,9 @@ angular.module('myApp')
       });
     },
     link: function postLink($scope, $element, $attrs) {
+      if(!$attrs.src){
+        setImageSrc($attrs, conf, conf.noImageSrc);
+      }
       $element.bind('error', function() {
         setImageSrc($attrs, conf, conf.brokenImageSrc);
       });
@@ -25,7 +28,7 @@ function setImageSrc($attrs, conf, src){
   var options = getOptions($attrs, conf, src);
   if(!srcHasResizimageApplied(conf, options)){
     if(options.size){
-      var imageSrc = src ? src : options.src;
+      var imageSrc = src || options.src;
       var resizimageUrl = getResizimageUri(conf, options.size, imageSrc);
       $attrs.$set("src", resizimageUrl);
     }    
@@ -43,16 +46,9 @@ function srcHasResizimageApplied(conf, options){
 
 function getOptions($attrs, conf, source){
   var options = {};
-  var src = undefined;
   if(isValidSize($attrs.resizimage)) options.size = $attrs.resizimage;
   if(isValidSize($attrs.size)) options.size = $attrs.size;
-  if(source){
-    options.src = source;
-  } else {
-    if($attrs.ngSrc) src = $attrs.ngSrc;
-    if($attrs.src) src = $attrs.src;
-    options.src = src ? src : conf.noImageSrc;
-  }
+  options.src = source || $attrs.src || $attrs.ngSrc || conf.noImageSrc;
   return options;
 }
 
